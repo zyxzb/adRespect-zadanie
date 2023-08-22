67da148d2724f9d251f0f40e70c9f50c559a5468
Bartek Z <bartekzarzyk@gmail.com>
2023-08-22 19:05:04 +0200

    add macy.js, loader and fix displaying new images

--- gallery.js
+++ gallery.js
@@ -1,9 +1,44 @@
-const thumbnails = document.querySelectorAll('.thumbnail img');
+let thumbnails = document.querySelectorAll('.thumbnail img');
 const popup = document.querySelector('.popup');
 const popupClose = document.querySelector('.popup-close');
 const popupImg = document.querySelector('.popup-img');
 const arrowLeft = document.querySelector('.popup-arrow-left');
 const arrowRight = document.querySelector('.popup-arrow-right');
+const loader = document.getElementById('loader');
+
+// MACY.JS
+
+var macyInstance = Macy({
+  container: '#gallery',
+  trueOrder: false,
+  waitForImages: false,
+  columns: 4,
+  margin: {
+    x: 40,
+    y: 40,
+  },
+  breakAt: {
+    1536: {
+      columns: 3,
+      margin: {
+        x: 30,
+        y: 30,
+      },
+    },
+    768: {
+      columns: 2,
+      margin: {
+        x: 20,
+        y: 20,
+      },
+    },
+    576: {
+      columns: 1,
+    },
+  },
+});
+
+// GALLERY
 
 let currentImageIndex;
 
@@ -22,7 +57,6 @@ const prevSlide = () => {
   } else {
     currentImageIndex -= 1;
   }
-  console.log(currentImageIndex);
   popupImg.src = thumbnails[currentImageIndex].src;
 };
 
@@ -30,19 +64,23 @@ const hidePopup = () => {
   popup.classList.add('hidden');
 };
 
-thumbnails.forEach((thumbnail, index) => {
-  const showPopup = (e) => {
-    popup.classList.remove('hidden');
-    popupImg.src = e.target.src;
-    currentImageIndex = index;
-  };
-  thumbnail.addEventListener('click', showPopup);
-  thumbnail.addEventListener('keydown', (e) => {
-    if (e.code === 'Enter' || e.KeyCode === 13) {
-      showPopup(e);
-    }
+const reloadThumbnails = (thumbnails) => {
+  thumbnails.forEach((thumbnail, index) => {
+    const showPopup = (e) => {
+      popup.classList.remove('hidden');
+      popupImg.src = e.target.src;
+      currentImageIndex = index;
+    };
+    thumbnail.addEventListener('click', showPopup);
+    thumbnail.addEventListener('keydown', (e) => {
+      if (e.code === 'Enter' || e.KeyCode === 13) {
+        showPopup(e);
+      }
+    });
   });
-});
+};
+
+reloadThumbnails(thumbnails);
 
 arrowRight.addEventListener('click', nextSlide);
 arrowLeft.addEventListener('click', prevSlide);
@@ -102,10 +140,47 @@ const displayImages = (images) => {
   });
 };
 
+// new images after fetching
+const attachListenersToThumbnails = () => {
+  const newThumbnails = document.querySelectorAll('.thumbnail img');
+  newThumbnails.forEach((thumbnail, index) => {
+    const showPopup = (e) => {
+      popup.classList.remove('hidden');
+      popupImg.src = e.target.src;
+      currentImageIndex = index;
+    };
+    thumbnail.addEventListener('click', showPopup);
+    thumbnail.addEventListener('keydown', (e) => {
+      if (e.code === 'Enter' || e.keyCode === 13) {
+        showPopup(e);
+      }
+    });
+  });
+};
+
+const showLoader = () => {
+  loader.classList.remove('invisible', 'opacity-0');
+  loader.classList.add('visible', 'opacity-100');
+};
+
+const hideLoader = () => {
+  loader.classList.remove('visible', 'opacity-100');
+  loader.classList.add('invisible', 'opacity-0');
+};
+
 const loadMoreImages = async () => {
+  showLoader();
   const images = await fetchImages();
   displayImages(images);
   page++;
+
+  attachListenersToThumbnails();
+  thumbnails = document.querySelectorAll('.thumbnail img');
+
+  setTimeout(() => {
+    macyInstance.recalculate();
+    hideLoader();
+  }, 1000);
 };
 
 showMoreBtn.addEventListener('click', loadMoreImages);
